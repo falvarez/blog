a39c7de6550aa38e5f8cee918efb6e3cec6b2f39
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2017-09-11 22:02:55 +0200

    Update deployment scripts to new hosting

--- bin/linux/deploy.sh
+++ bin/linux/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avze 'ssh' --delete output_prod/ digitalocean:~/webapps/blog.falvarez.es/public_html
+rsync -avze 'ssh' --delete output_prod/ hetzner:~/webapps/blog.falvarez.es/public_html
--- bin/osx/deploy.sh
+++ bin/osx/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avze 'ssh' --iconv=utf-8-mac,utf-8 --delete output_prod/ digitalocean:~/webapps/blog.falvarez.es/public_html
+rsync -avze 'ssh' --iconv=utf-8-mac,utf-8 --delete output_prod/ hetzner:~/webapps/blog.falvarez.es/public_html

70c79589da77e19fe7a247adcc77a3743eef96ca
Fede J. Álvarez <falvarez@bitban.com>
2023-10-10 08:42:45 +0200

    Exclude .bak files from deploy

--- bin/osx/deploy.sh
+++ bin/osx/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avze 'ssh' --iconv=utf-8-mac,utf-8 --exclude=".DS_Store" --delete output_prod/ hetzner:~/webapps/blog.falvarez.es/public_html
+rsync -avze 'ssh' --iconv=utf-8-mac,utf-8 --exclude=".DS_Store" --exclude="*.bak" --delete output_prod/ hetzner:~/webapps/blog.falvarez.es/public_html
